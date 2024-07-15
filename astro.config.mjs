40b529ef5407e684900208a539f43fe427a93a25
Gaston <gastonalvarez18@outlook.com>
2024-07-15 07:30:18 -0300

    archivos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import preact from "@astrojs/preact";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), preact()],
+  output: "server",
+  adapter: vercel(),
+  envPrefix: 'PUBLIC_',
+});
